8a7cbc82773490e6a41875818efae4da36927826
David <dprimenko@protonmail.com>
2017-02-18 20:49:19 +0100

    Fixed student put

--- config/routes.php
+++ config/routes.php
@@ -110,8 +110,7 @@ $app->put('/alumno/[{id}]', function($request, $response, $args) {
 		$result->setMessage("Error: ".$e->getMessage());
 	}
 
-	return $input['nombre']."->".$input['apellidos'];
-	//return $this->response->withJson($result).$input['nombre'];
+	return $this->response->withJson($result);
 });
 
 $app->delete('/alumno/[{id}]', function($request, $response, $args) {
