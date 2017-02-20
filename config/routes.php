f4438336ae6ffb25e922827ed4d898c5356ad7a1
David <dprimenko@protonmail.com>
2017-02-20 18:59:32 +0100

    Fixing get manager

--- config/routes.php
+++ config/routes.php
@@ -180,7 +180,7 @@ $app->post('/manager/getmng', function($request, $response, $args) {
 	$result = new ManagerResult();
 
 	try {
-
+		echo "SELECT * FROM ".TABLE_MANAGER." WHERE fecha = ".$args['fecha'];
 		$statement = $this->db->prepare("SELECT * FROM ".TABLE_MANAGER." WHERE fecha = ".$args['fecha']);
 		$statement->execute();
 		$manager = $statement->fetchAll();
