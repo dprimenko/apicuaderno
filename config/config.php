eaa1d0ea7175a277d939b362d93745ac4dee3135
David <dprimenko@protonmail.com>
2017-03-09 00:45:52 +0100

    Another

--- config/config.php
+++ config/config.php
@@ -84,17 +84,7 @@ class AlumnosResult {
 
 }
 
-class EmailResult() {
-	var $message;
 
-	function getMessage() {
-		return $this->message;
-	}
-
-	function setMessage($msg) {
-		$this->message = $msg;
-	}
-}
 
 class ManagerResult {
 
