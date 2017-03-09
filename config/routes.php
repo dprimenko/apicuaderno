6fa3b739e8f9243ddef67c21042238be9c81a775
David <dprimenko@protonmail.com>
2017-03-09 01:31:22 +0100

    Another

--- config/routes.php
+++ config/routes.php
@@ -1,6 +1,7 @@
 <?php
 
-require_once('phpmailer524/class.phpmailer.php');
+require 'phpmailer524/class.phpmailer.php';
+require 'phpmailer524/class.phpmailer.php';
 //include("class.smtp.php"); // optional, gets called from within class.phpmailer.php if not already loaded
 
 $app->get('/', function($request,$response) {
@@ -287,8 +288,7 @@ $app->put('/manager/[{idAlumno}]', function($request, $response, $args) {
 	return $this->response->withJson($result);
 });
 
-$app->post('/email', function($request, $response, $args) {
-	/*
+$app->post('/email', function($request, $response, $args) {	
 	$result = new EmailResult();
 
 	$input = $request->getParsedBody();
@@ -307,22 +307,20 @@ $app->post('/email', function($request, $response, $args) {
 	$mail->IsSMTP(); // telling the class to use SMTP
 
 	try {
-	 //$mail->SMTPDebug = 2; // enables SMTP debug information (for testing)
 	 $mail->SMTPDebug = 0;
 	 $mail->SMTPAuth = true; // enable SMTP authentication
 	 $mail->SMTPSecure = "tls"; // sets the prefix to the server
 	 $mail->Host = "smtp.gmail.com"; // sets GMAIL as the SMTP server
-	 //$mail->Host = "smtp.openmailbox.org";
 	 $mail->Port = 587; // set the SMTP port for the GMAIL server
 	 $mail->Username = $from; // GMAIL username
 	 $mail->Password = $password; // GMAIL password
+	 $mail->IsSendmail();
 	 $mail->AddAddress($to); // Receiver email
-	 $mail->SetFrom($from, 'David Primenko'); // email sender
 	 $mail->AddReplyTo($from, 'David Primenko'); // email to reply
 	 $mail->Subject = $subject; // subject of the message
-	 $mail->AltBody = 'Message in plain text'; // optional - MsgHTML will create an alternate automatically
+	 $mail->AltBody = 'Message in plain text'; // optional - MsgHTML will create an alternate automatically	
 	 $mail->MsgHTML($message); // message in the email
-	 //$mail->AddAttachment('images/phpmailer.gif'); // attachment
+	 $mail->IsHTML(true);
 	 $mail->Send();
 	 $result->setMessage("Message Sent OK to " . $to);
 	} catch (phpmailerException $e) {
@@ -333,59 +331,6 @@ $app->post('/email', function($request, $response, $args) {
 	 $result->setMessage("Error: " . $e->getMessage());	 
 	}
 
-	return $this->response->withJson($result);*/
-
-	//Create a new PHPMailer instance
-$result = new EmailResult();
-
-$mail = new PHPMailer;
-//Tell PHPMailer to use SMTP
-$mail->isSMTP();
-//Enable SMTP debugging
-// 0 = off (for production use)
-// 1 = client messages
-// 2 = client and server messages
-$mail->SMTPDebug = 2;
-//Ask for HTML-friendly debug output
-$mail->Debugoutput = 'html';
-//Set the hostname of the mail server
-$mail->Host = 'smtp.gmail.com';
-// use
-// $mail->Host = gethostbyname('smtp.gmail.com');
-// if your network does not support SMTP over IPv6
-//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
-$mail->Port = 587;
-//Set the encryption system to use - ssl (deprecated) or tls
-$mail->SMTPSecure = 'tls';
-//Whether to use SMTP authentication
-$mail->SMTPAuth = true;
-//Username to use for SMTP authentication - use full email address for gmail
-$mail->Username = "davidruso47@gmail.com";
-//Password to use for SMTP authentication
-$mail->Password = "david4769";
-//Set who the message is to be sent from
-$mail->setFrom('davidruso47@gmail.com', 'First Last');
-//Set an alternative reply-to address
-$mail->addReplyTo('replyto@example.com', 'First Last');
-//Set who the message is to be sent to
-$mail->addAddress('davidruso6@gmail.com', 'David Primenko');
-//Set the subject line
-$mail->Subject = 'PHPMailer GMail SMTP test';
-//Read an HTML message body from an external file, convert referenced images to embedded,
-//convert HTML into a basic plain-text alternative body
-//$mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
-//Replace the plain text body with one created manually
-$mail->AltBody = 'This is a plain-text message body';
-//Attach an image file
-//$mail->addAttachment('images/phpmailer_mini.png');
-//send the message, check for errors
-	if (!$mail->send()) {
-	    $result->setMessage("Error");
-	} else {
-	    $result->setMessage("Message Sent OK");
-	}	
-});
-
-return $this->response->withJson($result);
+	return $this->response->withJson($result);
 
 ?>
